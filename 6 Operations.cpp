204aef8282524195f0611c9a1f8d52acdbbeaffc
Nafisa <nrakhmonova@yandex.com>
2023-07-27 16:13:11 +0500

    6 Operations.cpp

--- 6 Operations.cpp
+++ 6 Operations.cpp
@@ -1,3 +1,7 @@
+/* Напишите программу, которая запрашивает у пользователя два числа и операцию
+(+, -, *, /), которую он хочет выполнить над этими числами. Используя оператор
+switch, выполните выбранную операцию и выведите результат на экран */
+
 #include <iostream>
 
 using namespace std;
@@ -8,15 +12,15 @@ int main()
     char oper;
  
     cout << "Введите первое число: "; cin >> a;
-    cout << "Введите первое число: "; cin >> b;
     cout << "Введите операцию: "; cin >> oper;
+    cout << "Введите второе число: "; cin >> b;
  
     switch (oper)
 	{
-	    case '+' : cout << a+b; break;
-	    case '-' : cout << a-b; break;
-	    case '*' : cout << a*b; break;
-	    case '/' : cout << a/b; break;
+	    case '+' : cout << a + b; break;
+	    case '-' : cout << a - b; break;
+	    case '*' : cout << a * b; break;
+	    case '/' : cout << a / b; break;
 	}
 
     return 0;
