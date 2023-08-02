41ec6d491d27d63576aec618490a7d99a8c813cf
Nafisa <nrakhmonova@yandex.com>
2023-08-03 01:38:08 +0500

    Add files via upload

---
+++ 8 Calculator.cpp
@@ -0,0 +1,68 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+// сложение
+float addition(float a, float b) 
+{
+    float result = a + b;
+    return result;
+}
+// вычитание
+float subtraction(float a, float b) 
+{
+    float result = a - b;
+    return result;
+}
+// умножение
+float multiplication(float a, float b)
+{
+    float result = a * b;
+    return result;
+}
+// деление
+float division(float a, float b) 
+{
+    float result = a / b;
+    return result;
+}
+// возведение в степень
+float exponentiation(float a, float b) 
+{
+    float result = pow(a,b);
+    return result;
+}
+
+int main()
+{
+    float a, b;
+    char oper;
+ 
+    cout << "Введите первое число: ";
+    cin >> a;
+    cout << "Введите операцию: "; 
+    cin >> oper;
+    cout << "Введите второе число: "; 
+    cin >> b;
+ 
+    switch (oper)
+	{
+	    case '+' :
+	        cout << addition(a, b);
+	        break;
+	    case '-' :
+	        cout << subtraction(a, b);
+	        break;
+	    case '*' :
+	        cout << multiplication(a, b);
+	        break;
+	    case '/' :
+	        cout << division(a, b);
+	        break;
+	    case '^' :
+	        cout << exponentiation(a, b);
+	        break;
+	}
+
+    return 0;
+}
